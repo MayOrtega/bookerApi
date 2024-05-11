609c7ca129aeead419a2a25d4f6049564ce25dba
MayOrtega <mayorlen.ortega@gmail.com>
2024-05-10 22:03:00 -0400

    Creacion componente reservas e importaciones

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/service/auth.service';
 
 @Component({
@@ -11,12 +12,12 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit(): void {
     this.authService.autenticar(this.username, this.password)
     .subscribe(() => {
-      // Redirección
+      this.router.navigate(['/reservas']);
       console.log('Autenticación exitosa');
     },
     error => {
